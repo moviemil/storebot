b39c50b9fbd58b151c89d46ffb51a6174fa5e039
moviemil <134944648+moviemil@users.noreply.github.com>
2023-10-14 12:02:40 -0300

    index.php

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 $token = '6661652395:AAHfT1yL4QePWkUJ5_Z6GyTTse5-TQ9oPlk';
 $apiUrl = "https://api.telegram.org/bot$token";
-$appUrl = 'https://fmlcoringa.000webhostapp.com/index.php';
+$appUrl = 'https://storebot.vercel.app/api/index.php';
 
 function setWebhook() {
   global $apiUrl, $appUrl;
